8f28891619cae14d47e92910cc3c2f96fd7a2d3f
JvPelai <jv.pelai@gmail.com>
2021-05-24 15:50:01 -0300

    initial commit

---
+++ src/br/com/alura/Curso.java
@@ -0,0 +1,15 @@
+package br.com.alura;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Curso {
+    private String nome;
+    private String instrutor;
+    private List<Aula> aulas = new LinkedList<Aula>();
+
+    public Curso(String nome, String instrutor) {
+
+    }
+
+}
